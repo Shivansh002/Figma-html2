cb2b48bf65f860557dacadf6a883ba1d3ba80548
Shivansh Nayak <shivanshnayak555@gmail.com>
2022-12-20 19:56:10 +0530

    First commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+ $(document).ready(function() {
+ 
+    $(".testimonials-init").owlCarousel({
+      loop: false,
+      navs : true, 
+      dots: true,
+      items : 1, 
+      itemsDesktop : false,
+      itemsDesktopSmall : false,
+      itemsTablet: false,
+      itemsMobile : false,
+ 
+  });
+});
